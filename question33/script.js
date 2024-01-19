4563abea5442871460a79366f8ec5527f2410f16
Raichel David <raicheldavid@techversantinfotech.com>
2024-01-19 21:45:51 +0530

    committed

---
+++ question33/script.js
@@ -0,0 +1,26 @@
+function perfectFunction() {
+   
+   var num=parseFloat(document.getElementById("num").value);
+        if (num<=0) {
+            alert("Please enter positive numbers");
+            return;
+        }
+let sum=0;
+   for(let i=1;i<num;i++){
+	if(num%i==0){
+		sum+=i;
+}
+
+				
+}
+if(sum==num){
+document.getElementById('displayText').innerHTML=document.getElementById('num').value +" is a perfect number";
+}
+else{
+document.getElementById('displayText').innerHTML=document.getElementById('num').value +" is not a perfect number";
+}
+
+
+}
+
+
